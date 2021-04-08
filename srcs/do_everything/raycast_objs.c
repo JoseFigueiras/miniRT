29b4c0677fc0742416fce1d36816ff77657dfe55
Josefig <josefiggaming@gmail.com>
2021-04-08 16:12:41 +0100

    remove stolen code

--- srcs/do_everything/raycast_objs.c
+++ srcs/do_everything/raycast_objs.c
@@ -10,12 +10,13 @@ int	raycast_objs(t_line line, t_scene scene)
 
 	color = 0;
 	color_temp = 0;
-	distance = 0;
+	distance = HUGE_VAL;
 	distance_temp = 0;
 	current = scene.objlst;
 	while (current)
 	{
 		distance_temp = raycast(line, scene, current, &color_temp);
+		//printf("|%f|\n", distance_temp);
 		if (distance_temp <= distance)
 		{
 			color = color_temp;
--- srcs/do_everything/raycast_plane.c
+++ srcs/do_everything/raycast_plane.c
@@ -3,19 +3,5 @@
 float		raycast_plane(t_line line, t_scene scene,
 							t_plane *plane, int *color)
 {
-	float	t;
-	t_xyz	intersect;
-	t_xyz	cam_coords;
 
-	cam_coords = scene.camlst->coords;
-	t = -1 * ((line.point.y - plane->coords.y) / line.vec.y);
-	if (t > 0 && t != HUGE_VAL)
-	{
-		intersect.x = (line.point.x + line.vec.x) * t;
-		intersect.y = (line.point.y + line.vec.y) * t;
-		intersect.z = (line.point.z + line.vec.z) * t;
-		*color = plane->color;
-		return (points_distance(cam_coords, intersect));
-	}
-	return (0);
 }
